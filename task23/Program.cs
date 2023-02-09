8f990df0dc3c9544a95cbdefad4525a8e00decbc
alehey <alexey.p.svyz@mail.ru>
2023-02-09 11:23:22 +0700

    add task 23

---
+++ task23/Program.cs
@@ -0,0 +1,26 @@
+﻿int InputIntNumber(string numberName)
+{
+    Console.Write($"Input {numberName} integer number: ");
+    int number;
+
+    while(!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("You inputed something wrong! Try again.");
+        Console.Write($"Input {numberName} integer number: " );
+    }
+    return number;
+}
+
+int a = InputIntNumber("luboe");
+int [] array = new int [Math.Abs(a)];
+for (int i = 1; i <= Math.Abs(a); i++)
+{
+    System.Console.WriteLine(i*i*i);
+    array[i-1] = i * i * i;
+
+}
+for (int i = 0; i < Math.Abs(a); i++)
+{
+    System.Console.Write($" {array[i]} ");
+
+}
